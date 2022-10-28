68e26f05ac99c3b2dc826c420dea2355e4a0d735
dd <pkh_dd@photons.com.tw>
2022-10-28 10:33:20 +0800

    將登入及註冊API區分為前後台

--- api/model/user.js
+++ api/model/user.js
@@ -1,23 +1,27 @@
 const Status = require('../../common/status');
 const Api = require('../api');
 const MySql = require('../../mysql/mysql');
-const Logger = require('../../logger/logger');
 
 class User extends Api {
+    #frontTable = 'front_user'
+    #backTable = 'back_user'
+
     constructor(app) {
         super();
-        app.post('/user/register', this.#register.bind(this));
-        app.post('/user/login', this.#login.bind(this));
+        app.post('/user/front/register', this.#frontRegister.bind(this));
+        app.post('/user/front/login', this.#frontLogin.bind(this));
+        app.post('/user/back/register', this.#backRegister.bind(this));
+        app.post('/user/back/login', this.#backLogin.bind(this));
         app.post('/user/testToken', this.#testToken.bind(this));
     }
 
-    async #register(req, res) {
+    async #register(req, res, table) {
         const status = this.#registerCheck(req);
         if (status != Status.Success) {
             return this.send(req, res, { status });
         }
 
-        const sql = `INSERT INTO user (user_id, password) VALUES ('${req.body.userId}', '${req.body.password}')`;
+        const sql = `INSERT INTO ${table} (user_id, password) VALUES ('${req.body.userId}', '${req.body.password}')`;
         const rows = await MySql.query(sql);
         if (!rows) {
             return this.send(req, res, { status: Status.Register_Fail });
@@ -26,15 +30,23 @@ class User extends Api {
         this.send(req, res, { status });
     }
 
-    async #login(req, res) {
+    async #frontRegister(req, res) {
+        this.#register(req, res, this.#frontTable);
+    }
+
+    async #backRegister(req, res) {
+        this.#register(req, res, this.#backTable);
+    }
+
+    async #login(req, res, table) {
         const status = this.#loginCheck(req);
         if (status != Status.Success) {
             return this.send(req, res, { status });
         }
 
-        const sql = `SELECT password from user WHERE user_id = '${req.body.userId}'`;
+        const sql = `SELECT password from ${table} WHERE user_id = '${req.body.userId}'`;
         const rows = await MySql.query(sql, Status.Login_Fail);
-        if (!rows) {
+        if (!rows || rows.length == 0) {
             return this.send(req, res, { status: Status.Login_Fail });
         }
 
@@ -48,6 +60,14 @@ class User extends Api {
             token: token
         }
         this.send(req, res, result);
+    }
+
+    #frontLogin(req, res) {
+        this.#login(req, res, this.#frontTable);
+    }
+
+    #backLogin(req, res) {
+        this.#login(req, res, this.#backTable);
     }
 
     #testToken(req, res) {
